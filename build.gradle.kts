6be6efd97f215c4c9f05bb5051b0ef5088b170b5
Andreas Ανδρέου <andreas.andreou@masabi.com>
2023-05-08 12:54:23 +0100

    Add gradle and build stuff

---
+++ build.gradle.kts
@@ -0,0 +1,28 @@
+plugins {
+    kotlin("jvm") version "1.8.20"
+    application
+}
+
+group = "com.pittacode"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation(kotlin("test"))
+    testImplementation("io.kotest:kotest-assertions-core:5.5.4")
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
+
+kotlin {
+    jvmToolchain(17)
+}
+
+application {
+    mainClass.set("MainKt")
+}
