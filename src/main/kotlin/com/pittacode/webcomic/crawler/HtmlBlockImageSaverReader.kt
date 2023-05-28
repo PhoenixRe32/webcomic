16e7ae7827b74559a31d57c6bd658a2404d0eb03
Andreas Ανδρέου <andreas.andreou@masabi.com>
2023-05-28 18:48:42 +0100

    Refactor processing img links and saving them Extract methods

--- src/main/kotlin/com/pittacode/webcomic/crawler/HtmlBlockImageSaverReader.kt
+++ src/main/kotlin/com/pittacode/webcomic/crawler/HtmlBlockImageSaverReader.kt
@@ -1,25 +1,42 @@
 package com.pittacode.webcomic.crawler
 
-import java.io.File
 import java.net.URL
 import java.nio.file.Files
 import java.nio.file.Path
+import java.nio.file.Paths
+import kotlin.io.path.exists
+import kotlin.io.path.nameWithoutExtension
 
 class HtmlBlockImageSaverReader {
     fun processAndSave(filePath: Path) {
         val blockOfHtmlSource: String = readFile(filePath)
-        processAndSave(blockOfHtmlSource)
+        processAndSave(blockOfHtmlSource, filePath.nameWithoutExtension)
     }
 
-    fun processAndSave(blockOfHtmlSource: String) {
+    fun processAndSave(blockOfHtmlSource: String, parentDirectoryName: String) {
+        val parentDirectory = create(parentDirectoryName)
+
         extractImgLinksInOrder(blockOfHtmlSource)
+            .filterNot { parentDirectory.contains(it) }
             .map { imgDlLink ->
                 val imageData = URL(imgDlLink.link.value).readBytes()
                     .also { println("Read bytes of ${imgDlLink.link}") }
-                File(imgDlLink.fileName.value).writeBytes(imageData)
+
+                val fileToCreate = parentDirectory.resolve(imgDlLink.fileName.value).toFile()
+                fileToCreate.writeBytes(imageData)
                     .also { println("Wrote bytes of ${imgDlLink.fileName}") }
             }
     }
+
+    private fun create(directory: String): Path {
+        val parentDirectory = Paths.get(directory)
+        return parentDirectory
+            .takeIf { it.exists() } // use path it if it exists
+            ?: Files.createDirectory(parentDirectory) // otherwise create it and then use it
+    }
+
+    private fun Path.contains(imgDlLink: ImgDlLink): Boolean =
+        resolve(imgDlLink.fileName.value).exists()
 
     private fun readFile(filePath: Path): String = Files.readString(filePath)
     // TODO figure out why resource isn't working
