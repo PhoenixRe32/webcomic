041c46d44eed50dc1c1e88cba72ddec81c3baa32
Andreas Ανδρέου <andreas.andreou@masabi.com>
2023-05-29 07:39:15 +0100

    WIP

--- src/main/kotlin/com/pittacode/webcomic/crawler/Main.kt
+++ src/main/kotlin/com/pittacode/webcomic/crawler/Main.kt
@@ -3,6 +3,11 @@ package com.pittacode.webcomic.crawler
 import kotlin.io.path.Path
 
 fun main(args: Array<String>) {
-    val htmlBlockFile = Path(args[0])
-    HtmlBlockImageSaverReader().processAndSave(htmlBlockFile)
+//    val htmlBlockFile = Path(args[0])
+    HtmlBlockImageSaverReader().processAndSave(Path("/Users/andreasandreou/Projects/webcomic-crawler/src/main/resources/Doctor Who - The Forgotten 01.html"))
+    HtmlBlockImageSaverReader().processAndSave(Path("/Users/andreasandreou/Projects/webcomic-crawler/src/main/resources/Doctor Who - The Forgotten 02.html"))
+    HtmlBlockImageSaverReader().processAndSave(Path("/Users/andreasandreou/Projects/webcomic-crawler/src/main/resources/Doctor Who - The Forgotten 03.html"))
+    HtmlBlockImageSaverReader().processAndSave(Path("/Users/andreasandreou/Projects/webcomic-crawler/src/main/resources/Doctor Who - The Forgotten 04.html"))
+    HtmlBlockImageSaverReader().processAndSave(Path("/Users/andreasandreou/Projects/webcomic-crawler/src/main/resources/Doctor Who - The Forgotten 05.html"))
+    HtmlBlockImageSaverReader().processAndSave(Path("/Users/andreasandreou/Projects/webcomic-crawler/src/main/resources/Doctor Who - The Forgotten 06.html"))
 }
