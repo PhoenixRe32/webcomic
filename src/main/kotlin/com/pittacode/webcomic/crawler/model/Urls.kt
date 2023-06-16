84036025faa2ac7041da43c7dabf6bdd06b83be8
Andreas Ανδρέου <andreas.andreou@masabi.com>
2023-06-16 22:53:00 +0100

    Creating URL takes care to escape weird characters in path

--- src/main/kotlin/com/pittacode/webcomic/crawler/model/Urls.kt
+++ src/main/kotlin/com/pittacode/webcomic/crawler/model/Urls.kt
@@ -1,16 +1,19 @@
 package com.pittacode.webcomic.crawler.model
 
+import java.net.URI
 import java.net.URL
 
 sealed class Url(private val value: String) {
     init {
-        require(value.startsWith("http")) { "Not a valid URL" }
+        require(value.startsWith("http")) { "Not a valid URL [$value]" }
     }
 
     val urlString
         get() = value
-    val url
-        get() = URL(value)
+    val url: URL
+        get() = with(URL(value)) {
+            return URI(protocol, userInfo, host, port, path, query, ref).toURL() // escapes characters this way
+        }
     val lastPathSegment: String
         get() =
             if (value.last() == '/') value.dropLast(1).substringAfterLast("/")
