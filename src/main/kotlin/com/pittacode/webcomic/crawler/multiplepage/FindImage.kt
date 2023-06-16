ae353522700613b1b5e49b31a83d82bc97ca1931
Andreas Ανδρέου <andreas.andreou@masabi.com>
2023-06-16 21:38:57 +0100

    Add log extension to result of image finder

--- src/main/kotlin/com/pittacode/webcomic/crawler/multiplepage/FindImage.kt
+++ src/main/kotlin/com/pittacode/webcomic/crawler/multiplepage/FindImage.kt
@@ -2,9 +2,18 @@ package com.pittacode.webcomic.crawler.multiplepage
 
 import com.pittacode.webcomic.crawler.model.ComicImage
 import com.pittacode.webcomic.crawler.model.PageUrl
+import mu.KotlinLogging
+
+private val logger = KotlinLogging.logger {}
 
 interface FindComicImages {
     fun on(page: PageUrl): List<ComicImage>
 }
 
-
+fun List<ComicImage>.log() {
+    when {
+        isEmpty() -> logger.warn { "Found no images" }
+        size > 1 -> logger.warn { "Found more than 1 image when looking for a div" }
+    }
+    logger.info { "Comic image links found: ${map { it.imgUrl.url }}" }
+}
